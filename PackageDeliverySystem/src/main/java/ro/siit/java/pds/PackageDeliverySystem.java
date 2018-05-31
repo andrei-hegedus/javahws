a5df50acd9cfa7cca434284d40c94f76f7bd9ed7
Andrei Hegedus <andrei.hegedus@scoalainformala.ro>
2018-05-31 19:36:56 +0300

    First class stubs.

---
+++ PackageDeliverySystem/src/main/java/ro/siit/java/pds/PackageDeliverySystem.java
@@ -0,0 +1,36 @@
+package ro.siit.java.pds;
+
+public class PackageDeliverySystem {
+
+    private Parcel packages[];
+
+    /**
+     * TODO
+     *
+     * @param pickupContactInfo
+     * @param deliveryContactInfo
+     * @return the tracking id of the delivery
+     */
+    public String requestDeliveryPickup(ContactInfo pickupContactInfo, ContactInfo deliveryContactInfo) {
+        return null;
+    }
+
+    /**
+     * TODO
+     * @param trackingId
+     * @return
+     */
+    public TrackingInfo getTrackingInfo(String trackingId){
+        return null;
+    }
+
+    /**
+     * TODO
+     * @param admin
+     * @return
+     */
+    public Parcel[] getPackages(Administrator admin){
+        return null;
+    }
+
+}
---
+++ PackageDeliverySystem/src/main/java/ro/siit/java/pds/Parcel.java
@@ -0,0 +1,9 @@
+package ro.siit.java.pds;
+
+public class Parcel {
+
+    private TrackingInfo trackingInfo;
+    private LogisticalInfo logisticalInfo;
+    private ContactInfo sender;
+    private ContactInfo destination;
+}
